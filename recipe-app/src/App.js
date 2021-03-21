35cc71db0e0286fa48f2e0b8d70bff5310014604
MistrDev <cwcorster123@gmail.com>
2021-03-21 09:19:05 -0500

    got my API fetch to work! and hid my API keys. Ahhh the small things really make a diffrence

--- recipe-app/src/App.js
+++ recipe-app/src/App.js
@@ -1,9 +1,31 @@
+import React,{useEffect, useState} from "react";
 import './App.css';
 
+const api_id = process.env.REACT_APP_APP_ID;
+const api_key = process.env.REACT_APP_APP_KEY;
+
+const [recipes, setRecipes] = useState([]);
+
 const App = () => {
+
+  useEffect( () => {
+  getRecipes();
+  }, []);
+
+  const getRecipes = async () => {
+    const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${api_id}&app_key=${api_key}`);
+    const data = await response.json()
+    setRecipes(data.hits);
+  }
+
   return(
     <div className="App">
-      <h1>Hello React</h1>
+      <form className='search-form'>
+        <input className="search-bar" type='text'/>
+        <button className="search-button" type="submit">
+         Search
+        </button>
+      </form>
     </div>
   )
 }
