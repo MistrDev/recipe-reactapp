a33e5bf74eb34c2559986624a8af849efbbcb2b3
MistrDev <cwcorster123@gmail.com>
2021-03-29 17:49:53 -0500

    ill have to come back to this app.. and debug it

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import './App.css';
 
 
 const App = () => {
-  const app_id = process.env.REACT_APP_API_ID;
-  const app_key = process.env.REACT_APP_API_KEY;
+  const API_ID = process.env.REACT_APP_API_ID;
+  const API_KEY = process.env.REACT_APP_API_KEY;
 
   const [recipes, setRecipes] = useState([]);
 
@@ -14,7 +14,7 @@ const App = () => {
   }, []);
 
   const getRecipes = async () => {
-    const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${app_id}&app_key=${app_key}`);
+    const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${API_ID}&app_key=${API_KEY}`);
     const data = await response.json()
     setRecipes(data.hits);
   }
